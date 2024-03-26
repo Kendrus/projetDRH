99446f0709109f3f66a2f07ed1263fe3af723251
kendra <sidoinelagrande@gmail.com>
2024-03-26 16:16:33 +0000

    first commit

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Auth;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(Auth::guard('Admin')->check()){
+           return redirect()->route('login_from')->with('error','Plz login First');
+        }
+
+
+
+
+        return $next($request);
+    }
+}
